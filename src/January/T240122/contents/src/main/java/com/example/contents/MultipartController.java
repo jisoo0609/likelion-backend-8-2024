73df3739a8fe44099264c51c8b20fc53a6bc090a
jisoo0609 <jisu20000609@gmail.com>
2024-01-22 15:28:38 +0900

    240122 File Handling - multipart

--- src/January/T240122/contents/src/main/java/com/example/contents/MultipartController.java
+++ src/January/T240122/contents/src/main/java/com/example/contents/MultipartController.java
@@ -7,24 +7,45 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
 // multipart/form-data를 받기 위한 연습
 @Slf4j
 @RestController
 public class MultipartController {
-    @PostMapping(value = "/multipart", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(
+            value = "/multipart",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
     public String multipart(
-            @RequestParam("name") String name,
-            // 받아주는 자료형을 MultipartFile로 설정
-            @RequestParam("file")MultipartFile multipartFile
+            @RequestParam("name")
+            String name,
+            // 받아주는 자료형을 MultipartFile
+            @RequestParam("file")
+            MultipartFile multipartFile
     ) throws IOException {
-        log.info(multipartFile.getOriginalFilename());
+        /*log.info(multipartFile.getOriginalFilename());
+        // TODO 폴더가 없을때 만들기
+
         // 파일을 저장할 경로 + 파일명 지정
         Path downloadPath = Path.of("media/" + multipartFile.getOriginalFilename());
-        // 저장한다
-        multipartFile.transferTo(downloadPath);
+        // 저장한다.
+        multipartFile.transferTo(downloadPath);*/
+        // 저장할 파일 이름
+        File file = new File("./media/" + multipartFile.getOriginalFilename());
+        try (OutputStream outputStream = new FileOutputStream(file)){
+            // byte[] 형태로 파일을 받는다.
+            byte[] fileBytes = multipartFile.getBytes();
+            // TODO 이미지 파일을 구성하는 byte[]가 맞는지 확인
+            System.out.println(new String(fileBytes, StandardCharsets.UTF_8));
+            outputStream.write(fileBytes);
+        }
+
         return "done";
     }
 }
