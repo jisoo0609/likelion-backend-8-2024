c81eeccd7f61e2cfe6b40a6e20d3d42c6c88efac
jisoo0609 <jisu20000609@gmail.com>
2024-01-31 15:38:36 +0900

    240131 BaseEntity 추가

---
+++ src/January/T240131/jpa-next/src/main/java/com/example/jpanext/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.jpanext.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+// 다른 Entity가 상속받아서 내가 가진 속성 정보를 포함시키고 싶을때
+// 상속 받는 Entity 클래스 임을 나타내는 Annotation
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
