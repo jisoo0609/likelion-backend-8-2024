7696efa46c8e5e4c640d21b8b4460ed18f76565e
jisoo0609 <jisu20000609@gmail.com>
2024-02-06 15:24:12 +0900

    240206 fetch 방법

---
+++ src/February/T240206/querydsl/src/test/java/com/example/querydsl/QuerydslQueryTests.java
@@ -0,0 +1,149 @@
+package com.example.querydsl;
+
+import com.example.querydsl.entity.Item;
+import com.example.querydsl.entity.QItem;
+import com.example.querydsl.entity.Shop;
+import com.example.querydsl.repo.ItemRepository;
+import com.example.querydsl.repo.ShopRepository;
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.example.querydsl.entity.QItem.item;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+public class QuerydslQueryTests {
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private ShopRepository shopRepository;
+    @Autowired
+    private JPAQueryFactory queryFactory;
+
+    // @BeforeEach: 각 테스트 전에 실행할 코드를 작성하는 영역
+    @BeforeEach
+    public void beforeEach() {
+        Shop shopA = shopRepository.save(Shop.builder()
+                .name("shopA")
+                .description("shop A description")
+                .build());
+        Shop shopB = shopRepository.save(Shop.builder()
+                .name("shopB")
+                .description("shop B description")
+                .build());
+
+        itemRepository.saveAll(List.of(
+                Item.builder()
+                        .shop(shopA)
+                        .name("itemA")
+                        .price(5000)
+                        .stock(20)
+                        .build(),
+                Item.builder()
+                        .shop(shopA)
+                        .name("itemB")
+                        .price(6000)
+                        .stock(30)
+                        .build(),
+                Item.builder()
+                        .shop(shopB)
+                        .name("itemC")
+                        .price(8000)
+                        .stock(40)
+                        .build(),
+                Item.builder()
+                        .shop(shopB)
+                        .name("itemD")
+                        .price(10000)
+                        .stock(50)
+                        .build(),
+                Item.builder()
+                        .name("itemE")
+                        .price(11000)
+                        .stock(10)
+                        .build(),
+                Item.builder()
+                        .price(10500)
+                        .stock(25)
+                        .build()
+        ));
+    }
+
+    @Test
+    public void fetch() {
+        // fetch(): 단순하게 전체 조회
+        List<Item> foundList = queryFactory
+                // SELECT FROM 절
+                .selectFrom(item)
+                // 결과를 리스트 형태로 조회
+                .fetch();
+        assertEquals(6, foundList.size());
+
+        // fetchOne(): 하나만 조회하려고 시도
+        Item found = queryFactory
+                .selectFrom(item)
+                .where(item.id.eq(1L))
+                // 하나만 조회
+                .fetchOne();
+        assertEquals(1L, found.getId());
+
+        found = queryFactory
+                .selectFrom(item)
+                .where(item.id.eq(0L))
+                // 없을 경우 null
+                .fetchOne();
+        assertNull(found);
+
+        assertThrows(Exception.class, () -> {
+            queryFactory.selectFrom(item)
+                    // 2개 이상일 경우 Exception
+                    .fetchOne();
+        });
+
+        // fetchFirst(): 첫번째 결과 또는 null
+        found = queryFactory
+                .selectFrom(item)
+                // LIMIT 1 -> fetchOne();
+                .fetchFirst();
+        assertNotNull(found);
+
+        // offset limit
+        foundList = queryFactory
+                .selectFrom(item)
+                .offset(3)
+                .limit(2)
+                .fetch();
+        for (Item find: foundList) {
+            System.out.println(find.getId());
+        }
+
+        // fetchCount(): 결과의 갯수 반환 (deprecated)
+        long count = queryFactory
+                .selectFrom(item)
+                .fetchCount();
+        assertEquals(6, count);
+
+        // fetchResults(): 결과 및 count + offset + limit 정보 반환 (deprecated)
+        QueryResults<Item> results = queryFactory
+                .selectFrom(item)
+                .offset(3)
+                .limit(2)
+                .fetchResults();
+        System.out.println(results.getTotal());
+        System.out.println(results.getOffset());
+        System.out.println(results.getLimit());
+        // 실제 내용은 getResults()
+        foundList = results.getResults();
+    }
+
+}
