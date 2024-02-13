3aa4abccb0aa304145494582ecf158c97b8f9779
jisoo0609 <jisu20000609@gmail.com>
2024-02-13 13:31:43 +0900

    240213 redis init

---
+++ src/February/T240213/redis/src/main/java/com/example/redis/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.example.redis.config;
+
+import com.example.redis.dto.PersonDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, PersonDto> personRedisTemplate(
+            RedisConnectionFactory connectionFactory
+    ) {
+        RedisTemplate<String, PersonDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // 주어진 데이터의 직렬화 방식을 결정
+        // Redis의 Value는 결국 문자열 형식이니까,
+        // 주어진 데이터(DTO)를 어떻게 문자열로 바꿀 것인지
+//        template.setDefaultSerializer(RedisSerializer.json());
+        // 미리 만들어진 String 변환기를 설정
+        template.setKeySerializer(RedisSerializer.string());
+        // 미리 만들어진 JSON 변환기를 설정
+        template.setValueSerializer(RedisSerializer.json());
+        return template;
+    }
+}
