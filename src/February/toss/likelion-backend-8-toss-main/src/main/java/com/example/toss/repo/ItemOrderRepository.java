ce98482c3c184e8de40c251087cdb202cb612660
jisoo0609 <jisu20000609@gmail.com>
2024-02-20 15:23:47 +0900

    240220 남은 요구사항을 위한 준비

---
+++ src/February/toss/likelion-backend-8-toss-main/src/main/java/com/example/toss/repo/ItemOrderRepository.java
@@ -0,0 +1,7 @@
+package com.example.toss.repo;
+
+import com.example.toss.entity.ItemOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemOrderRepository extends JpaRepository<ItemOrder, Long> {
+}
