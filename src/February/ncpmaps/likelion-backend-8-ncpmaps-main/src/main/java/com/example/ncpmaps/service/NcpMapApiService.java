9882bac8f252d1ca3653c898c6b66ba3ca15074a
jisoo0609 <jisu20000609@gmail.com>
2024-02-21 15:17:23 +0900

    240221 GeolocationClient test

--- src/February/ncpmaps/likelion-backend-8-ncpmaps-main/src/main/java/com/example/ncpmaps/service/NcpMapApiService.java
+++ src/February/ncpmaps/likelion-backend-8-ncpmaps-main/src/main/java/com/example/ncpmaps/service/NcpMapApiService.java
@@ -1,6 +1,8 @@
 package com.example.ncpmaps.service;
 
 import com.example.ncpmaps.dto.direction.DirectionNcpResponse;
+import com.example.ncpmaps.dto.geocoding.GeoNcpResponse;
+import com.example.ncpmaps.dto.rgeocoding.RGeoNcpResponse;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
@@ -12,6 +14,18 @@ public interface NcpMapApiService {
     // 이 interface와 Config를 조합해 RestClient를 사용하는 구현체를 만들어야함
     @GetExchange("/map-direction/v1/driving")
     DirectionNcpResponse directions5(
+            @RequestParam
+            Map<String, Object> params
+    );
+
+    @GetExchange("/map-geocode/v2/geocode")
+    GeoNcpResponse geocode(
+            @RequestParam
+            Map<String, Object> params
+    );
+
+    @GetExchange("/map-reversegeocode/v2/gc")
+    RGeoNcpResponse reverseGeocode(
             @RequestParam
             Map<String, Object> params
     );
