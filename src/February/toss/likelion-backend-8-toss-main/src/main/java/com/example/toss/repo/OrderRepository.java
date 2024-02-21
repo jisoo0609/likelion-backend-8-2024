833e3a6fcdf555fd9aea12db15801eaec54b220c
jisoo0609 <jisu20000609@gmail.com>
2024-02-21 09:50:37 +0900

    240221 주문 저장 및 주문 조회

--- src/February/toss/likelion-backend-8-toss-main/src/main/java/com/example/toss/repo/OrderRepository.java
+++ src/February/toss/likelion-backend-8-toss-main/src/main/java/com/example/toss/repo/OrderRepository.java
@@ -3,5 +3,5 @@ package com.example.toss.repo;
 import com.example.toss.entity.ItemOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemOrderRepository extends JpaRepository<ItemOrder, Long> {
+public interface OrderRepository extends JpaRepository<ItemOrder, Long> {
 }
