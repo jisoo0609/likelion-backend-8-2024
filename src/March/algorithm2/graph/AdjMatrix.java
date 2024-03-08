973733ee5c1bc4c503f6a56bdadaaa47b2540dee
jisoo0609 <jisu20000609@gmail.com>
2024-03-08 17:22:04 +0900

    240308 bfs

--- src/March/algorithm2/graph/AdjMatrix.java
+++ src/March/algorithm2/graph/AdjMatrix.java
@@ -3,10 +3,7 @@ package March.algorithm2.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class AdjMatrix {
     public static void dfsAdjMatrix() throws IOException {
@@ -133,6 +130,34 @@ public class AdjMatrix {
             adjMat[leftNode][rightNode] = 1;
             adjMat[rightNode][leftNode] = 1;
         }
+
+        // DFS에서 사용하던 Stack 대신 Queue를 사용한다.
+        Queue<Integer> toVisit = new LinkedList<>();
+        List<Integer> visitedOrder = new ArrayList<>();
+        // 방문한 정보를 저장한다.
+        boolean[] visited = new boolean[nodes];
+        // 첫 방문 대상은 0
+        toVisit.add(0);
+        // 큐가 빌때까지 반복한다.
+        while (!toVisit.isEmpty()) {
+            // 이번에 방문할 곳을 받아온다.
+            int next = toVisit.poll();
+            // 만약 방문했다면 스킵
+            if (visited[next]) continue;
+
+            // 방문했다고 표시한다.
+            visited[next] = true;
+            visitedOrder.add(next);
+
+            // 다음 방문할 정점을 확인한다.
+            for (int i = 0; i < nodes; i++) {
+                if (adjMat[next][i] == 1 && !visited[i]) {
+                    toVisit.offer(i);
+                }
+            }
+        }
+
+        System.out.println(visitedOrder);
     }
 
     public static void main(String[] args) throws IOException {
@@ -160,9 +185,9 @@ public class AdjMatrix {
 
         // 결과를 출력해보자.
         for (int i = 0; i < nodes; i++) {
-            System.out.println(Arrays.toString(adjMat[i]));
+            System.out.println(Arrays.toString(adjMat[i]))
         }
-*/
+ */
     }
 }
 
